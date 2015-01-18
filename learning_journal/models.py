f135dd55fb24257bad6b61177a64303ae2995102
darcy3e <darcy.e.balcarce@gmail.com>
2015-01-17 16:34:35 -0800

    edit entry done

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -1,10 +1,15 @@
+import datetime
 from sqlalchemy import (
     Column,
+    DateTime,
     Index,
     Integer,
     Text,
+    Unicode,
+    UnicodeText,
     )
 
+import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 
 from sqlalchemy.orm import (
@@ -25,3 +30,34 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Unicode(255), unique=True, nullable=False)
+    body = Column(UnicodeText, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @classmethod
+    def all(cls, session=None):
+        """return a query with all entries, ordered by creation date reversed
+        """
+        if session is None:
+            session = DBSession
+        return session.query(cls).order_by(sa.desc(cls.created)).all()
+
+    @classmethod
+    def by_id(cls, id, session=None):
+        """return a single entry identified by id
+        If no entry exists with the provided id, return None
+        """
+        if session is None:
+            session = DBSession
+        return session.query(cls).get(id)
+
+
+
+
+
